671d9406a5923a7cce59fadb53848bfe0a2f4f96
Roni Juppi <roju269@gmail.com>
2021-09-08 12:14:56 +0300

    Document bg_gen.py

--- bg_gen.py
+++ bg_gen.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+Generates a gray background of size 32x32 and saves it as bg.png
+
+Requirements:
+  Pillow library from pip: https://pypi.org/project/Pillow/
+
+  pip install Pillow
+"""
 
 from PIL import Image
 
